6ec46b4bb705af8d58008deffaf105e8ce6ec75c
Sameer yadav <sameer.manoj2005@gmail.com>
2026-07-21 15:09:03 +0530

    vercel.json

--- server/gateway/index.js
+++ server/gateway/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(morgan('dev'))
 app.use(express.json());
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://flux-gamma-opal.vercel.app',
     methods:['GET','POST'],
     credentials:true
 }))
